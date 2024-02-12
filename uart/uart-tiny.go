0009bb7dba79baeed5d803e0d2e9b7fb35700543
Scott Feldman <sfeldma@gmail.com>
2024-02-11 22:25:37 -0800

    fix uart

--- uart/uart-tiny.go
+++ uart/uart-tiny.go
@@ -5,11 +5,11 @@ package uart
 import "machine"
 
 type Uart struct {
-	machine.UART0
+	*machine.UART
 }
 
 func New() Uart {
-	u := Uart{}
+	u := Uart{machine.UART0}
 	u.Configure(machine.UARTConfig{
 		TX:       machine.UART0_TX_PIN,
 		RX:       machine.UART0_RX_PIN,
