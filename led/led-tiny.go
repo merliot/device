7e4acc895baaa496bb9b5876fc844c3918b0b36f
Scott Feldman <sfeldma@gmail.com>
2024-02-21 18:08:21 -0800

    add gpio to led

--- led/led-tiny.go
+++ led/led-tiny.go
@@ -2,15 +2,23 @@
 
 package led
 
-import "machine"
+import (
+	"machine"
+
+	"github.com/merliot/device/target"
+)
 
 type Led struct {
+	Gpio  string
 	State bool
 	pin   machine.Pin
 }
 
 func (l *Led) Configure() {
 	l.pin = machine.LED
+	if pin, ok := target.Pin(l.Gpio); ok {
+		l.pin = machine.Pin(pin)
+	}
 	l.pin.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	l.pin.Low()
 }
