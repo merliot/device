de1854d2cedbc05401de4860d9e39ae4a4f7c08b
Scott Feldman <sfeldma@gmail.com>
2024-04-03 15:26:53 -0700

    add DHT demo

---
+++ dht/dht-demo.go
@@ -0,0 +1,37 @@
+//go:build !tinygo && !rpi
+
+package dht
+
+import (
+	"math"
+	"math/rand"
+	"time"
+)
+
+type Dht struct {
+	Gpio        string
+	Temperature float32
+	Humidity    float32
+}
+
+func (d *Dht) Configure() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomValue(mean, stddev float64) float32 {
+	u1 := rand.Float64()
+	u2 := rand.Float64()
+
+	// Box-Muller transform to generate normally distributed random numbers
+	z0 := math.Sqrt(-2.0*math.Log(u1)) * math.Cos(2.0*math.Pi*u2)
+	value := mean + stddev*z0
+
+	// Round to 1 dec place
+	return float32(math.Round(value*10) / 10)
+}
+
+func (d *Dht) Read() error {
+	d.Temperature = randomValue(75.1, 0.05)
+	d.Humidity = randomValue(34.5, 0.05)
+	return nil
+}
